9b1e7ce83bad0c1dee85f86118f4a78e566773fb
Mike Ronald Fuentes <mikeronald.fuentes@cit.edu>
2022-12-16 13:12:44 +0800

    Rename LeapYear Components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,31 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
 
-public class LeapYearGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+public class LeapYearGUI extends JFrame{
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+    private JPanel jpanel;
+
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        LeapYearGUI leap = new LeapYearGUI();
+        leap.setContentPane(leap.jpanel);
+        leap.setSize(400,300);
+        leap.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        leap.setVisible(true);
+    }
+
 }
+
+
